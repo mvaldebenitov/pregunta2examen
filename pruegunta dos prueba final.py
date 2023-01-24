f75f05c9d3ae888cb0ca8698fe1bb94e4a49f5fa
‘mvaldebenitov’ <mauricio.valdebenito.valdivia@gmail.com>
2023-01-24 04:39:02 +0000

    codigo circunferencia2

--- pruegunta dos prueba final.py
+++ pruegunta dos prueba final.py
@@ -3,3 +3,7 @@ r = float(input("Escriba valor del radio: "))
 area = 3.141592 * r ** 2
 
 print("El area del circulo es igual a: {:.6f}".format(area))
+
+circunferencia = 2 * 3.141592 * r
+
+print("La circunferencia del circulo es: {:.6f}".format(circunferencia))
